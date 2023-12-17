e024af6a57c24ec26143d47c45ebdb4470b8d8c9
Hagarabdelhamed <83194017+Hagarabdelhamed@users.noreply.github.com>
2023-12-17 02:31:47 +0200

    add function to check weather condition

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,59 @@ class WeatherApp extends StatelessWidget {
     //provide Cubit
     return  BlocProvider(
       create: (context) => GetWeattherCubit(),
-      child: const MaterialApp(
-        home: HomeView(),
+      child: MaterialApp(
+        theme: ThemeData(
+          primarySwatch: getThemeColor(),
+
+        ),
+        home: const HomeView(),
       ),
     );
   }
+  
+  MaterialColor getThemeColor(String condition) {
+  switch (condition) {
+    case 'Sunny':
+      return Colors.orange;
+    case 'Partly cloudy':
+    case 'Cloudy':
+    case 'Overcast':
+      return Colors.grey;
+    case 'Mist':
+      return Colors.blueGrey;
+    case 'Patchy rain possible':
+    case 'Patchy snow possible':
+    case 'Patchy sleet possible':
+    case 'Patchy freezing drizzle possible':
+      return Colors.lightBlue;
+    case 'Thundery outbreaks possible':
+      return Colors.deepPurple;
+    case 'Blowing snow':
+    case 'Blizzard':
+      return Colors.blueGrey;
+    case 'Fog':
+    case 'Freezing fog':
+      return Colors.grey;
+    case 'Patchy light drizzle':
+    case 'Light drizzle':
+      return Colors.brown;
+    case 'Freezing drizzle':
+    case 'Heavy freezing drizzle':
+      return Colors.blue;
+    case 'Patchy light rain':
+    case 'Light rain':
+    case 'Moderate rain at times':
+    case 'Moderate rain':
+      return Colors.lightGreen;
+    case 'Heavy rain at times':
+    case 'Heavy rain':
+      return Colors.green;
+    case 'Light freezing rain':
+    case 'Moderate or heavy freezing rain':
+      return Colors.blue;
+    // Add more conditions as needed
+    default:
+      return Colors.blue;
+  }
+}
 }
